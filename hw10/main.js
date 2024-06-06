50475884463efc2c4aa8cb92e01ba2912473f8ee
peterwang0329 <peterwang95267@gmail.com>
2024-06-06 15:59:50 +0800

    updata

--- hw10/main.js
+++ hw10/main.js
@@ -1,41 +1,37 @@
 const main = document.querySelector('main')
 
-let key = "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"
+let key = "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"
 
 //與groq交互的函式
 async function groqChat(q,language) {
-    //try{
+    try{
     const jsonResponse = await fetch("https://api.groq.com/openai/v1/chat/completions", 
     {
         body: JSON.stringify({
             "model": "llama3-8b-8192",  //使用模型
-            "messages": [
-                {"role":'system',"content": language},
-                {"role": "user", "content": q}
-            ],   //使用者輸入的訊息
+            "messages": [{"role": "user", "content":"How to say "+ q + " in " + language}],   //使用者輸入的訊息
             "temperature": 0.7  //設定溫度參數
         }),
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json', //請求的類型
-            'Authorization': `Bearer ${key}`,   //key
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${key}`,
         }
     })
     const jsonData = await jsonResponse.json()
     console.log(JSON.stringify(jsonData, null, 2))
     return jsonData.choices[0].message.content
-    /*}
+    }
     catch(error){
         console.error("Error:",error);
         return "請求失敗,請稍後再試";
-    }*/
+    }
 }
 
 //回應函式
 async function chat() {
-    let source = document.getElementById('source')
     let target = document.getElementById('target')
-    let language = 'Translate from ' + source.value + ' to ' + target.value
+    let language = target.value
     let qNode = document.querySelector('#question')
     let responseNode = document.querySelector('#response')
     responseNode.innerText = '翻譯中，請稍等幾秒鐘 ...'
